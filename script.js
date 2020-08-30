6703fe7cb141ff47154e00deed9193515481d1f3
Reed Santos <reedsantos@icloud.com>
2020-08-29 21:17:38 -0400

    Add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Declare variables
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var uppercaseArray = uppercase.split("");
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -7,13 +8,16 @@ var numbersArray = numbers.split("");
 var special = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
 var specialArray = special.split("");
 
+// Use user input to create a password
 function passwordGenerate() {
     var resultArray = [];
     var resultString = "";
+    // User input to select length of password
     var length = prompt("Choose a length for your password between 8 and 128 characters.");
     if (length < 8 || length > 128) {
         alert("That number was not between 8 and 128.");
     }
+    // Push user conditions into an empty array
     else {
         if (confirm("Will your password contain uppercase letters?")) {
             Array.prototype.push.apply(resultArray, uppercaseArray);
@@ -35,11 +39,13 @@ function passwordGenerate() {
             alert("You must select at lease 1 type of condition to generate a password!");
         }
         else {
+            // Set the legth of the final password
             for (var i = 0; i < length; i++) {
                 var random = Math.floor(Math.random() * resultArray.length);
                 resultString += resultArray[random];
             }
         }
     }
+    // Returns password in text area
     document.getElementById("password").innerHTML = resultString;
 }
