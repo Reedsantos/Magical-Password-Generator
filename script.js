e09a750ca08e446081126f2bed3358286910ad92
Reed Santos <reedsantos@icloud.com>
2020-08-29 20:54:07 -0400

    Add prompts

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var special = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+
+
+function passwordGenerate() {
+    var length = prompt("Choose a length for your password between 8 and 128 characters.");
+    if (length < 8 || length > 128) {
+        alert("That number was not between 8 and 128.");
+    }
+    else {
+        if (confirm("Will your password contain uppercase letters?")) {
+        }
+
+        if (confirm("Will your password contain lowercase letters?")) {
+        }
+
+        if (confirm("Will your password contain numbers?")) {
+        }
+
+        if (confirm("Will your password contain special characters?")) {
+        }
+
+    }
+};
